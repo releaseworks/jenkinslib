556f10fbdd0d6feb2e6e50be5b5f06be4589ae0d
Miiro Juuso <miiro.juuso@gmail.com>
2019-03-07 14:18:37 +0000

    syntax

--- src/releaseworks/Slack.groovy
+++ src/releaseworks/Slack.groovy
@@ -1,19 +1,20 @@
 package org.releaseworks
 
-def webhookUrl = 'https://release.works'
+class Slack {
+    String webhookUrl = 'https://release.works'
 
-def message(channel, text) {
-    def endpoint = new URL(webhookUrl)
-    def queryString = 'v=test'
-    def connection = endpoint.openConnection()
-    connection.with {
-      doOutput = true
-      requestMethod = 'POST'
-      outputStream.withWriter { writer ->
-        writer << queryString
-      }
-      println content.text
+    def message(channel, text) {
+        def endpoint = new URL(webhookUrl)
+        def queryString = 'v=test'
+        def connection = endpoint.openConnection()
+        connection.with {
+          doOutput = true
+          requestMethod = 'POST'
+          outputStream.withWriter { writer ->
+            writer << queryString
+          }
+          println content.text
+        }
     }
-}
 
-return this
+}
