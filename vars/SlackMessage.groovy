fba91de10a283d6d951dc89caea5fcd02e803581
Miiro Juuso <miiro.juuso@gmail.com>
2019-03-07 16:02:44 +0000

    add exception for missing webhook url

--- vars/SlackMessage.groovy
+++ vars/SlackMessage.groovy
@@ -1,3 +1,15 @@
 def call(Map config) {
-  sh "curl -XPOST -s -d 'payload={ \"color\": \"good\", \"channel\": \"${config.channel}\", \"text\": \"${config.message}\" }' ${config.webhookUrl}"
+  
+  if (!config.containsKey('webhookUrl')) {
+    throw new Exception("Missing webhookUrl")
+  }
+
+  sh """
+  curl -XPOST -s -d 'payload={ \
+    \"color\": \"${config.get('color', 'good')}\", \
+    \"channel\": \"${config.get('channel', '')}\", \
+    \"username\": \"${config.get('username', 'Jenkins')}\", \
+    \"text\": \"${config.message}\" \
+    }' ${config.webhookUrl}
+  """
 }
