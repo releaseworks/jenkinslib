67ff56ab7e172e2e95f29711973b77edd045c788
Miiro Juuso <miiro.juuso@gmail.com>
2019-03-07 15:21:04 +0000

    readme

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Place this to the top of your Jenkinsfile:
 ```
 @Library('github.com/releaseworks/jenkinslib') _
 ```
+## Functions
+### SlackMessage
+Example:
+```
+SlackMessage(webhookUrl: "https://hook.slack.com/XXXX",
+             channel: "#ci",
+             message: "Build succeeded!")
+```
--- vars/SlackMessage.groovy
+++ vars/SlackMessage.groovy
@@ -1,4 +1,4 @@
 def call(Map config) {
   def endpoint = new URL(config.webhookUrl)
-  sh "curl -XPOST -d 'payload={ \"color\": \"good\", \"text\": \"${config.message}\" }' ${config.webhookUrl}"
+  sh "curl -XPOST -d 'payload={ \"color\": \"good\", \"channel\": \"${config.channel}\" \"text\": \"${config.message}\" }' ${config.webhookUrl}"
 }
