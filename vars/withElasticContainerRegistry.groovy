18db2257b16cbe5009039c8ed5d3357972b88ea0
Miiro Juuso <miiro.juuso@gmail.com>
2019-03-09 12:26:00 +0000

    fix withECR

--- vars/withElasticContainerRegistry.groovy
+++ vars/withElasticContainerRegistry.groovy
@@ -1,7 +1,9 @@
 def call(Closure body) {
     try {
-        docker.image("releaseworks/awscli:latest").inside("--entrypoint \"\" -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -v /var/run/docker.sock:/var/run/docker.sock") {
-            sh "$(aws ecr get-login | sed 's/-e none //')"
+        // Need to mount the Docker socket, and host's /etc/passwd and /etc/group for sudo to work
+        docker.image("releaseworks/awsdockercli:latest").inside("--entrypoint \"\" -e HOME=. -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_DEFAULT_REGION -v /var/run/docker.sock:/var/run/docker.sock -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro") {
+            sh "sudo docker_socket_init"
+            sh "\$(aws ecr get-login | sed 's/-e none //')"
             body()
         }
     } catch( ClassNotFoundException e ) {
