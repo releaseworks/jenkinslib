0a2e707cbdbf69517178646f7dd8dab5120ec8f2
Miiro Juuso <miiro.juuso@gmail.com>
2019-11-12 10:33:38 +0000

    replace double quotes with &quote; in message

--- vars/SlackMessage.groovy
+++ vars/SlackMessage.groovy
@@ -4,12 +4,15 @@ def call(Map config) {
     throw new Exception("Missing webhookUrl")
   }
 
+  message = config.message.replaceAll("&", "&amp;")
+  message = message.replaceAll("\"", "&quote;")
+
   sh """
   curl -XPOST -s -d 'payload={ \
     \"color\": \"${config.get('color', 'good')}\", \
     \"channel\": \"${config.get('channel', '')}\", \
     \"username\": \"${config.get('username', 'Jenkins')}\", \
-    \"text\": \"${config.message.replaceAll("&", "&amp;")}\" \
+    \"text\": \"${message}\" \
     }' ${config.webhookUrl}
   """
 }
